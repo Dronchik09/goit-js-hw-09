2b7446000ab210df4ce209642de520f66475999b
Andriy_Chornobay <andriy.chornobay@gmail.com>
2024-06-25 13:02:05 +0300

    add initial files

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+
+document.querySelector("#app").innerHTML = `
+<ul class="home-ul">
+      <li>
+        <a class="home-link" href="./1-gallery.html"
+          >Gallery</a>
+      </li>
+      <li>
+        <a class="home-link" href="./2-form.html">Form</a>
+      </li>
+    </ul>
+`;
